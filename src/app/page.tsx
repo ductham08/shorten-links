6993ccef16f0d90ac2dd34402c73c1b5d3406ef0
ductham08 <ductham784@gmail.com>
2025-06-12 02:03:59 +0700

    created ui shortener page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { SiteHeader } from "@/components/site-header"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 import {
     SidebarInset,
     SidebarProvider,
@@ -20,8 +22,11 @@ export default function Page() {
                 <SiteHeader path="Home" />
                 <div className="flex flex-1 flex-col">
                     <div className="@container/main flex flex-1 flex-col gap-2">
-                        <div className="p-4">
-                            <p>hhhuhebhgevhgv</p>
+                        <div className="p-4 flex justify-center">
+                            <div className="flex w-full max-w-sm items-center gap-2">
+                                <Input type="text" placeholder="Enter your url" className="focus-visible:ring-0" />
+                                <Button type="submit" variant="outline"> Shorten </Button>
+                            </div>
                         </div>
                     </div>
                 </div>
