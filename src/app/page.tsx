9982716472e13c929ed850f0ddce857af45c9410
ductham08 <ductham784@gmail.com>
2025-06-12 01:57:01 +0700

    created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import { AppSidebar } from "@/components/app-sidebar"
-import { ChartAreaInteractive } from "@/components/chart-area-interactive"
-import { SectionCards } from "@/components/section-cards"
 import { SiteHeader } from "@/components/site-header"
 import {
     SidebarInset,
     SidebarProvider,
 } from "@/components/ui/sidebar"
-
 
 export default function Page() {
     return (
