8170a42fd2561f708f0b81d63e3032e53f09c8c4
ductham08 <ductham784@gmail.com>
2025-07-16 22:58:02 +0700

    create ui register page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,15 @@
+import { RegisterForm } from '@/components/register-form'
+import { GalleryVerticalEnd } from 'lucide-react'
+import React from 'react'
+
+const RegisterPage = () => {
+    return (
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+            <div className="flex w-full max-w-sm flex-col gap-6">
+                <RegisterForm />
+            </div>
+        </div>
+    )
+}
+
+export default RegisterPage
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,23 @@
 import mongoose, { Schema } from 'mongoose';
 
 const userSchema = new Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['user', 'admin'], default: 'user' },
-    refreshToken: { type: String },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    role: { 
+        type: String, 
+        enum: ['user', 'admin'], 
+        default: 'user' 
+    },
+    refreshToken: { 
+        type: String 
+    },
 }, { timestamps: true });
 
 export default mongoose.models.User || mongoose.model('User', userSchema);
